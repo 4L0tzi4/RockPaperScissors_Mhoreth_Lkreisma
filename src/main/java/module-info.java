a715b94bb50e02c41f518480dda43ffa9c035e77
kreis <lkreisma@htl-steyr.ac.at>
2025-11-24 16:01:01 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module htl.steyr.rockpaperscissors_mhoreth_lkreisma {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens htl.steyr.rockpaperscissors_mhoreth_lkreisma to javafx.fxml;
+    exports htl.steyr.rockpaperscissors_mhoreth_lkreisma;
+}
