b62fefa55a48419eed31e10b216f3436b8d57988
kreis <lkreisma@htl-steyr.ac.at>
2025-11-25 11:06:41 +0100

    added new Class Computer.java() mit der funktion chosenWeapon -> gibt int zurück

---
+++ src/main/java/htl/steyr/rockpaperscissors_mhoreth_lkreisma/Computer.java
@@ -0,0 +1,12 @@
+package htl.steyr.rockpaperscissors_mhoreth_lkreisma;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Computer {
+
+    public int chosenWeapon(){
+
+        return ThreadLocalRandom.current().nextInt(1, 3 + 1);
+    }
+}
