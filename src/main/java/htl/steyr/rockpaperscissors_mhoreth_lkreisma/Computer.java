78f58d219a3c07e3d01e02d0ec09661cd0c4b5e9
Manuel Horeth <mhoreth@htl-steyr.ac.at>
2025-11-26 13:33:45 +0100

    changed names of pngs and buttons inside the fxml. Also made changes to the Computer.java

--- src/main/java/htl/steyr/rockpaperscissors_mhoreth_lkreisma/Computer.java
+++ src/main/java/htl/steyr/rockpaperscissors_mhoreth_lkreisma/Computer.java
@@ -4,8 +4,8 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Computer {
 
-    public int chosenWeapon(){
-
+    public int chosenWeapon() {
         return ThreadLocalRandom.current().nextInt(1, 3 + 1);
+
     }
 }
